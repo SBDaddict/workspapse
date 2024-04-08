86a1f2d80967028ddb842bde5dddc593cf3e424e
SBDaddict <najoon1102@gmail.com>
2024-04-09 07:34:14 +0900

    2024-04-09

--- Java_Variable/src/ch5/ArrayEx1_5_10.java
+++ Java_Variable/src/ch5/ArrayEx1_5_10.java
@@ -5,27 +5,25 @@ import java.util.Arrays;
 public class ArrayEx1_5_10 {
     public static void main(String[] args){
         int[] numArr = new int[10];
-
-        for(int i = 0; i < numArr.length;i++){
-            System.out.print(numArr[i]= (int)(Math.random()*10));
+        for(int i = 0 ; i < numArr.length;i++){
+            numArr[i] = (int)(Math.random()*10);
+            System.out.print(numArr[i]);
         }System.out.println();
-
-        for(int i = 0;i < numArr.length-1;i++){
-            boolean change = false;
-            for(int j = 0 ; j < numArr.length-1-i;j++){
-                if(numArr[j]>numArr[j+1]){
-                    int temp = numArr[j];
+        for(int i = 0 ; i < numArr.length-1;i++){
+            boolean stop = false;
+            for(int j = 0; j < numArr.length-1-i;j++){
+                if(numArr[j] > numArr[j+1]){
+                    int tmp = numArr[j];
                     numArr[j] = numArr[j+1];
-                    numArr[j+1] = temp;
-                    change = true;
+                    numArr[j+1] = tmp;
+                    stop = true;
+                    }
                 }
-            }//for j and
-            if(!change)break;
-            for(int k = 0 ; k < numArr.length;k++){
+            if(!stop)break;
+            for(int k = 0 ; k < numArr.length ; k++)
                 System.out.print(numArr[k]);
-            }
             System.out.println();
-        }//for i and
+        }// j for and
+    }// i for and
     }//main and
 
-}
