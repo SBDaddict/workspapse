64faddb5b5442c28a36f43e4d42fccf7756a94cc
SBDaddict <najoon1102@gmail.com>
2024-05-10 08:39:47 +0900

    20240510

---
+++ Java_Variable/src/practiceExercises/Ch_6_1_SutdaCard.java
@@ -0,0 +1,32 @@
+package practiceExercises;
+
+class SutdaCard {
+	boolean isKwang = false;
+	private int num = 0;
+	String kwang = " ";
+	SutdaCard(){}
+	public void setNum(int num){
+		if(num <= 0 || num <=10) {
+		this.num = num;
+		}
+	}
+	public int getNum() {
+return num;	}
+	boolean kwang() {
+
+		if (kwang.equals("광"))
+			return isKwang = true;
+			return false;
+	}
+}
+
+public class Ch_6_1_SutdaCard {
+
+	public static void main(String[] args) {
+		SutdaCard s = new SutdaCard();
+		s.setNum(11);
+		System.out.println(s.getNum());
+		
+	}
+
+}
